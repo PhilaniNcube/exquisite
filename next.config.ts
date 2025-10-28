2b0817cb20cbac52c6c700391725e34f452dad53
PhilaniNcube <ncbphi001@gmail.com>
2025-10-28 14:30:18 +0200

    Pre Next.js 16 upgrade commit

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,20 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-    experimental: {
+  experimental: {
     viewTransition: true,
   },
   typedRoutes: true,
-  images:{
+  images: {
     qualities: [25, 50, 70, 75, 80, 85, 90, 95, 100],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'pub-6c1050288c7041a9a3c730794fa669ba.r2.dev',        
+        protocol: "https",
+        hostname: "pub-6c1050288c7041a9a3c730794fa669ba.r2.dev",
       },
     ],
-  }
+  },
 };
 
 export default withPayload(nextConfig);
