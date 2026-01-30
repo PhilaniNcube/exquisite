c4dc1f484910120ee8a406cdc3f1c86e283a9edb
PhilaniNcube <ncbphi001@gmail.com>
2026-01-30 17:47:34 +0200

    fix: Enhance logoutAction to revalidate path and redirect after logout

--- src/app/(public)/(account)/logout/logoutAction.ts
+++ src/app/(public)/(account)/logout/logoutAction.ts
@@ -2,13 +2,17 @@
 
 import { logout } from '@payloadcms/next/auth'
 import config from '@payload-config'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
 export async function logoutAction() {
   try {
-    return await logout({ allSessions: true, config })
+    await logout({ allSessions: true, config })
+    revalidatePath('/', 'layout')
   } catch (error) {
     throw new Error(
       `Logout failed: ${error instanceof Error ? error.message : 'Unknown error'}`,
     )
   }
+  redirect('/')
 }
